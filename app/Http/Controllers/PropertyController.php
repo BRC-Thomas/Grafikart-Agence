6c1b86b98c938114f9ac9e5386c50efff865814b
Thomas <thomasbaracani@gmail.com>
2023-06-02 22:40:30 +0200

    Listing home page

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -2,18 +2,49 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\SearchPropertiesRequest;
 use App\Models\Property;
 use Illuminate\Http\Request;
 
 class PropertyController extends Controller
 {
-    public function index()
+    public function index(SearchPropertiesRequest $request)
     {
+        $query = Property::query()->orderBy('created_at','desc');
+        if($request->validated('price')) {
+            $query = $query->where('price','<=',$request->validated('price'));
+        }
 
+        $query = Property::query();
+        if($request->validated('surface')) {
+            $query = $query->where('surface','>=',$request->input('surface'));
+        }
+
+        $query = Property::query();
+        if($request->validated('rooms')) {
+            $query = $query->where('rooms','>=',$request->input('rooms'));
+        }
+
+        $query = Property::query();
+        if($request->validated('title')) {
+            $query = $query->where('title','like',"%{$request->input('title')}%");
+        }
+
+        return view('properties.index', [
+            'properties' => $query->paginate(16),
+            'input'=> $request->validated()
+        ]);
     }
 
     public function show(string $slug, Property $property)
     {
+        $expectedSlug = $property->getSlug();
+        if ($slug !== $expectedSlug ){
+            return to_route('properties.show',['slug'=>$expectedSlug, 'property'=>$property]);
+        }
 
+        return view('properties.show',[
+            'property'=>$property
+        ]);
     }
 }
---
+++ resources/views/properties/index.blade.php
@@ -0,0 +1,37 @@
+@extends('base')
+
+@section('title', 'Tous nos biens')
+
+@section('content')
+
+    <div class="bg-light p-5 mb-5 text-center">
+        <form action="" method="get" class="container d-flex gap-2">
+            <input type="number" name="surface" placeholder="Surface minimum" class="form-control" value="{{ $input['surface'] ?? '' }}" >
+            <input type="number" name="rooms" placeholder="Nombre de pièces minimum" class="form-control" value="{{ $input['rooms'] ?? '' }}" >
+            <input type="number" name="rooms" placeholder="Nombre de pièces minimum" class="form-control" value="{{ $input['rooms'] ?? '' }}" >
+            <input type="text" name="title" placeholder="Mot clef" class="form-control" value="{{ $input['title'] ?? '' }}" >
+            <button class="btn-primary-btn-sm-flex-grow-0">
+                Rechercher
+            </button>
+        </form>
+    </div>
+
+    <div class="container">
+        <div class="row mb-4">
+            @forelse($properties as $property)
+                <div class="col-3">
+                    @include('properties.card')
+                </div>
+            @empty
+                <div class="col">
+                    Aucun bien ne correspond à votre recherche
+                </div>
+            @endforelse
+        <div class="my-4">
+            {{ $properties->links() }}
+        </div>
+    </div>
+
+
+
+@endsection
